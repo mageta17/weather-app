dc4d104cb6ea94bb4b1a44c9b8768b908fb2e723
mageta sukulu <magetasukulu@gmail.com>
2025-06-06 15:11:07 +0300

    Auto commit

---
+++ frontend/src/components/BaseModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <Transition>
+        <div 
+            v-show="modalActive" 
+            class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+        >
+            <Transition>
+                <div 
+                    v-if="modalActive" 
+                    class="p-4 bg-white self-start mt-32 max-w-screen-md"
+                >
+                    <slot />
+                    <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('close-modal')">
+                        Close
+                    </button>
+                </div>
+            </Transition>            
+        </div>
+    </Transition>
+    
+</template>
+
+<script setup>
+defineEmits(['close-modal']);
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false,
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
