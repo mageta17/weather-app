a8268af103d0b23559ea86b5cf7dc2b6a0da9069
mageta sukulu <magetasukulu@gmail.com>
2025-06-07 01:02:45 +0300

    Auto commit

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,50 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input 
+        v-model="searchQuery"
+        @input="getSearchResults"
+        type="text" 
+        placeholder="Search for a city or state" 
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]" 
+      />
+      <ul 
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <li 
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer" 
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const mapboxAPIKey = "pk.eyJ1IjoibWFnZXRhIiwiYSI6ImNtYmxicW93djB3dnQyanIxeHl0ZjV6dDkifQ.aCwnxFV6GMAuk5gy0Zon0Q";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value); // Clear previous timeout if it exists
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`
+      );
+      mapboxSearchResults.value = result.data.features;
+      console.log(mapboxSearchResults.value);
+      return;
+    }
+    mapboxSearchResults.value = null; // Clear results if searchQuery is empty
+  }, 300); // Debounce time of 300ms
+};
+</script>
