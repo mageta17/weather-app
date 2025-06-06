d1a3dfc89e3a8ce6a9b0af719b9639c17189de5e
mageta sukulu <magetasukulu@gmail.com>
2025-06-06 15:23:33 +0300

    Auto commit

--- frontend/src/components/BaseModal.vue
+++ frontend/src/components/BaseModal.vue
@@ -4,7 +4,7 @@
             v-show="modalActive" 
             class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
         >
-            <Transition name="modal">
+            <Transition name="modal-inner">
                 <div 
                     v-if="modalActive" 
                     class="p-4 bg-white self-start mt-32 max-w-screen-md"
