2dca3f8d4561420d9f5ae7b911341c19c7ff29cf
mageta sukulu <magetasukulu@gmail.com>
2025-06-07 16:17:24 +0300

    Auto commit

--- frontend/src/components/AsyncCityView.vue
+++ frontend/src/components/AsyncCityView.vue
@@ -5,5 +5,36 @@
 </template>
 
 <script setup>
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const getWeatherData = async () => {
+    try {
+        const weatherData = await axios.get(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=c38bd32440189b8f19f5935322f76a62&units=imperial`
+        );
+
+        // cal current date & time
+        const localOffset = new Date().getTimezoneOffset() * 60000;
+        const utc = weatherData.data.current.dt * 1000 + localOffset;
+        weatherData.data.currentTime =
+            utc + 1000 * weatherData.data.timezone_offset;
+
+        // cal hourly weather offset
+        weatherData.data.hourly.forEach((hour) => {
+        const utc = hour.dt * 1000 + localOffset;
+        hour.currentTime =
+            utc + 1000 * weatherData.data.timezone_offset;
+        });
+
+        return weatherData.data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return null;
+    }
+};
+const weatherData = await getWeatherData();
+console.log(weatherData);
 
 </script>
